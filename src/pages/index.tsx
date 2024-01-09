d52281787dc7b4dfe3e006da09a66843991f7056
jillnguyen2311 <nguyennuduyhoai75@gmail.com>
2024-01-09 13:37:32 -0800

    add header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic"
 import Head from 'next/head'
+import Image from "next/image";
 
 const DynamicMap = dynamic(() => import ('../../components/Map'), {ssr: false});
 
@@ -10,7 +11,28 @@ export default function Home() {
       <title>Expo & Millennium Line map</title>
       <link rel="icon" href="/images/logo.png" />
     </Head>
+
+    <div style={headerStyle}>
+        <h1>Expo & Millennium Line stations map</h1>
+        <Image
+          src="/images/logo.png"
+          width={70}
+          height={70}
+          alt="logo"
+        />
+      </div>
     <DynamicMap />
   </div>
   )
 }
+
+const headerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor:'#E0DFDF',
+  gap: 10,
+  padding: '5px',
+  fontFamily: 'Arial, sans-serif',
+  fontSize: 25
+};
